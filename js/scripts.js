62d2852e9dc29ceb02fc890d95803ff5eda071e4
Oskar Wong <atorpos@mac.com>
2023-08-08 14:39:28 -0400

    nessessary file update

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,24 @@
-const html = document.documentElement;
-const canvas = document.getElementById("hero-lightpass");
-const context = canvas.getContext("2d");
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
 
-const frameCount = 118;
-const currentFrame = index => (
-    `images/Famicom${index.toString().padStart(3, '0')}.jpg`
-    // `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
-)
+function showSlides() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.transform = 'translateX(0)';
+        } else {
+            const distance = (index - slideIndex) * 100;
+            slide.style.transform = `translateX(${distance}%)`;
+        }
+    });
 
-const preloadImages = () => {
-    for (let i = 0; i < frameCount; i++) {
-        const img = new Image();
-        img.src = currentFrame(i);
+    slideIndex++;
+    console.log(slideIndex);
+    console.log(slides.length);
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
     }
-};
 
-const img = new Image()
-img.src = currentFrame(1);
-canvas.width=1158;
-canvas.height=770;
-img.onload=function(){
-    context.drawImage(img, 0, 0);
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
 }
 
-const updateImage = index => {
-    img.src = currentFrame(index);
-    context.drawImage(img, 0, 0);
-}
-
-window.addEventListener('scroll', () => {
-    const scrollTop = html.scrollTop;
-    const maxScrollTop = html.scrollHeight - window.innerHeight;
-    const scrollFraction = scrollTop / maxScrollTop;
-    const frameIndex = Math.min(
-        frameCount - 1,
-        Math.ceil(scrollFraction * frameCount)
-    );
-
-    requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
-
-preloadImages()
-
-const stickyText = document.querySelector('.sticky-text');
-const headerHeight = document.querySelector('header').offsetHeight;
-
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > headerHeight) {
-        stickyText.classList.add('sticky');
-    } else {
-        stickyText.classList.remove('sticky');
-    }
-});
+showSlides();
