5f01236ecdc437e0194f8668f327213fbb76069c
IanTeddy <136382277+IanTeddy@users.noreply.github.com>
2023-08-10 12:19:59 -0400

    Add files via upload

---
+++ companyInfo.js
@@ -0,0 +1,11 @@
+// When mouse over, change the image
+function mouseOn(){
+  var obj = document.getElementById("defalut-mario");
+  obj.src = "1200px-NSMB2_Invincible_Mario_Artwork.png";
+}
+ 
+//When mouse out, back to default image
+function mouseOff(){
+  var obj = document.getElementById("defalut-mario");
+  obj.src = "mariogroup.png";
+}
