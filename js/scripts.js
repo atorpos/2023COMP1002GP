3c26133c0a205521da9708908356963dfc7262f9
Oskar Wong <atorpos@mac.com>
2023-08-11 12:42:31 -0400

    nessessary file update

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
-
-function showSlides() {
-    slides.forEach((slide, index) => {
-        const offset = (index - slideIndex + slides.length) % slides.length;
-        slide.style.transform = `translateX(${offset * 100}%)`;
-    });
-
-    slideIndex = (slideIndex + 1) % slides.length;
-
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+var int;
+function setInt() {
+    clearInterval(int);
+    int = setInterval(function() {
+        var btns = document.getElementsByName("carousel");
+        for(var i = 0; i < btns.length; i++) {
+            if(btns[i].checked) {
+                btns[i].checked = false;
+                if(i + 1 == btns.length) {
+                    btns[0].checked = true;
+                }
+                else {
+                    btns[i + 1].checked = true;
+                }
+                return;
+            }
+        }
+    }, 5000);
 }
-
-showSlides();
+setInt();
