0c939ea80d51ff8185649ce0e182045869743dd1
Oskar Wong <atorpos@mac.com>
2023-08-11 10:46:43 -0400

    nessessary file update

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,11 @@ const slides = document.querySelectorAll('.slide');
 
 function showSlides() {
     slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-            slide.style.transform = 'translateX(0)';
-        } else {
-            const distance = (index - slideIndex) * 100;
-            slide.style.transform = `translateX(${distance}%)`;
-        }
+        const offset = (index - slideIndex + slides.length) % slides.length;
+        slide.style.transform = `translateX(${offset * 100}%)`;
     });
 
-    slideIndex++;
-    console.log(slideIndex);
-    console.log(slides.length);
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
-    }
+    slideIndex = (slideIndex + 1) % slides.length;
 
     setTimeout(showSlides, 3000); // Change slide every 3 seconds
 }
