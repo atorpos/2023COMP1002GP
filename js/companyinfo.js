b0dd82b156e15c4b53447c13e9128cf28985a2ab
Oskar Wong <atorpos@mac.com>
2023-08-13 13:23:07 -0400

    nessessary file update

--- js/companyinfo.js
+++ js/companyinfo.js
@@ -1,11 +1,11 @@
 // When mouse over, change the image
 function mouseOn(){
     var obj = document.getElementById("defalut-mario");
-    obj.src = "1200px-NSMB2_Invincible_Mario_Artwork.png";
+    obj.src = "images/1200px-NSMB2_Invincible_Mario_Artwork.png";
 }
 
 //When mouse out, back to default image
 function mouseOff(){
     var obj = document.getElementById("defalut-mario");
-    obj.src = "mariogroup.png";
+    obj.src = "images/mariogroup.png";
 }
